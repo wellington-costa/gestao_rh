5f1c77db3ed740be86552fb27ed5738956cb64d0
wellington-costa <wellington_costa@live.com>
2019-03-28 12:28:04 -0300

    Views delete Funcionario

--- apps/funcionarios/urls.py
+++ apps/funcionarios/urls.py
@@ -1,7 +1,12 @@
 from django.urls import path, include
-from .views import home
+from .views import CreateFuncionario, ListFuncionarios, UpadteFuncionarios, DeleteFuncionario
 
 urlpatterns = (
-    path('', home),
+
+    path('novo', CreateFuncionario.as_view(), name = 'create_funcionario'),
+    path('', ListFuncionarios.as_view(), name = 'list_funcionarios'),
+    path('editar/<int:pk>/', UpadteFuncionarios.as_view(), name = 'update_funcionario'),
+    path('deletar/<int:pk>/', DeleteFuncionario.as_view(), name = 'delete_funcionario'),
+
 
 )
--- apps/funcionarios/views.py
+++ apps/funcionarios/views.py
@@ -1,5 +1,28 @@
 from django.http import HttpResponse
-from django.shortcuts import render
+from django.urls import reverse, reverse_lazy
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from .models import Funcionario
+from django.views.generic.list import ListView
 
-def home(request):
-     return HttpResponse('Olá')
+class CreateFuncionario(CreateView):
+     model = Funcionario
+     fields = ['nome', 'usuario', 'departamento', 'empresa']
+
+class ListFuncionarios(ListView):
+     model = Funcionario
+
+     def get_queryset(self):
+          empresa_logada = self.request.user.funcionario.empresa
+          queryset = Funcionario.objects.filter(empresa = empresa_logada)
+          return queryset
+
+class UpadteFuncionarios(UpdateView):
+     model = Funcionario
+     fields = ['nome', 'departamento']
+
+class DeleteFuncionario(DeleteView):
+     model = Funcionario
+     success_url = reverse_lazy('list_funcionarios')
+
+
+
